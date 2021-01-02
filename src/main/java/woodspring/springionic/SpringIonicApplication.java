a2b9cb510e6afc9bd7376b2b64540e59c0c7971a
woodspring.toronto <woodspring.toronto@gmail.com>
2021-01-01 20:59:45 -0500

    first add spring boot

---
+++ src/main/java/woodspring/springionic/SpringIonicApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+}
